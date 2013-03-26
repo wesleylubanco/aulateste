2c6361cac2b958b66abe005fab68269e1204d09c
lab-cord <lab-cord@ama.(none)>
2013-03-26 10:08:46 -0300

    Inicio do projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aulateste::Application.config.secret_token = 'ed86fd49eb129e1d818bfd78d2048d2286eb95b1b652dac1dddb671cf4bdd2ad390a28d8f71d39562d7904b4198cf3ca31cf342ee0a7ec85736e1ff4ce9e2596'
